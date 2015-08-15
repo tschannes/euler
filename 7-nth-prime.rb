6f3c017455e1fdc3079ffda8c42b0818d81c2ef7
tschannes <mail@tscherrig.net>
2015-08-15 13:09:51 +0200

    slightly faster version of Prime Finder

--- 7-nth-prime.rb
+++ 7-nth-prime.rb
@@ -1,5 +1,4 @@
 require './Timer.rb'
-#require 'pry'
 
 class PrimeFinder
 
@@ -11,39 +10,23 @@ class PrimeFinder
 		super
 	end
 
-
-
 	def isPrime(x)
-		return false if x % 2 == 0
-		factors = []
-		counter = factors.length
+		factorsCounter = 0
 		n = 1
-		while n < x
-			if x % n == 0
-				factors.push(n)
-				counter += 1
-				n += 1
-			end
+		while n < x/2 && factorsCounter < 2
+			factorsCounter += 1 if x % n == 0
 			n += 1
 		end
-		if counter < 2
-			return true
-		else
-			return false
-		end
+		true if factorsCounter < 2
 	end
 
 	def findNthPrime
-		n = 2
-		while @primes.length < @limit
-			if isPrime(n)
-				@primes.push(n)
-				n += 1
-			else
-				n += 1
-			end
+		num = 1
+		while @primes.length < @limit + 1
+			@primes.push(num) if isPrime(num)
+			num += 2
 		end
-		puts @primes[-1].to_s + " is the " + @limit.to_s + "th Prime."
+		puts __method__.to_s + ": " + @primes[-1].to_s + " is the " + @limit.to_s + "th Prime."
 		return @primes[-1]
 	end
 
