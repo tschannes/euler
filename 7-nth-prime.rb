51369f09a69025a114efe3cc3b6ad00c32788c11
tschannes <mail@tscherrig.net>
2015-08-15 12:03:01 +0200

    slow but working primefinder

--- 7-nth-prime.rb
+++ 7-nth-prime.rb
@@ -7,71 +7,52 @@ class PrimeFinder
 
 	def initialize(nthElement)
 		@limit = nthElement
+		@primes = [2]
 		super
 	end
 
-	def nthElement
-		primes = []
-		num = 1
 
-		while primes.length < @limit
-			num += 1
-			x = 1
-			counter = 0
-			next if num % 2 == 0
-			while x < num
-				counter += 1 if num % x == 0
-				#binding.pry
-				x += 1
-				isPrime = false if counter > 2
-			end
-			primes.push(num) if isPrime 
-			puts "The " + primes.length.to_s + "th Prime is: " + primes[-1].to_s + "."
-		end
-		puts primes[-1]
-		return primes[-1]
-	end
 
 	def isPrime(x)
 		return false if x % 2 == 0
-		#factors = []
-		counter = 0
+		factors = []
+		counter = factors.length
 		n = 1
 		while n < x
 			if x % n == 0
-				#factors.push(n)
+				factors.push(n)
 				counter += 1
 				n += 1
 			end
-			return false if counter > 2
 			n += 1
 		end
-		return true
+		if counter < 2
+			return true
+		else
+			return false
+		end
 	end
 
 	def findNthPrime
-		i = 1
-		counter = 0
-		primes = [] 
-		while counter <= @limit
-			if isPrime(i)
-				primes.push(i)
-				counter += 1
-				puts "The " + primes.length.to_s + "th Prime is: " + primes[-1].to_s + "."
+		n = 2
+		while @primes.length < @limit
+			if isPrime(n)
+				@primes.push(n)
+				n += 1
+			else
+				n += 1
 			end
-			i += 2
 		end
-		puts primes[-1]
-		return primes[-1]
+		puts @primes[-1].to_s + " is the " + @limit.to_s + "th Prime."
+		return @primes[-1]
 	end
 
 	def run
-		#nthElement
 		findNthPrime
 		measure
 	end
 
 end
 
-x = PrimeFinder.new(6)
+x = PrimeFinder.new(2000)
 x.run
